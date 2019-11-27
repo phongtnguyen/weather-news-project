bd13105a2b99ed3a9154ffadf6774f76f7c506cb
Phong Nguyen <phong.nguyen1901@gmail.com>
2019-11-27 11:41:10 -0600

    Empty secrets

---
+++ weather-news/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  weather-news
+//
+//  Created by Phong Nguyen on 11/27/19.
+//  Copyright © 2019 Phong Nguyen. All rights reserved.
+//
+
+import Foundation
+
+struct APIKey {
+
+}
